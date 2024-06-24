01b3fdea837c4733e313c9960535e19a7c32beea
ricardo8santana <luisriccardo@proton.me>
2024-06-24 18:28:52 -0300

    jogo numero secreto commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1')
+titulo.innerHTML = 'Hora do desafio'
+
+function botaoFoiClicado(){
+    console.log('Eu amo JavaScript')
+}
+
+function EumoJavaScript(){
+    console.log('Eu amo JavaScript')
+}
